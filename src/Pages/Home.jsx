0f1bcd1c2c86bb9102a63d2cf2299c6c15d481f3
Alex Zimmerman <ZimmermanAlex13@gmail.com>
2023-05-10 23:59:06 -0400

    Buttons

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,7 @@
 import Header from '../Components/Header'
 import '../App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 
 function Home() {
@@ -13,8 +15,21 @@ function Home() {
       <div className="text">
         <h1>Hi, I'm Alex.</h1>
         <p>I'm a self-taught developer and Finance Major at the University of Massachusetts Amherst.</p>
+        <p>Checkout my Github or hit some of the button below to see what I'm up to.</p>
         {/*Link to my GitHub*/}
-        <a href="https://github.com/TakingJester766" target="_blank">GitHub</a>
+        <div className="buttons">
+        
+        <a href="https://github.com/TakingJester766" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faGithub} size="2x" id="github_logo" />
+        </a>
+
+        {/*Link to my LinkedIn*/}
+        <a href="https://www.linkedin.com/in/alex-zimmerman/" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} size="2x" id="linkedin_logo" />
+        </a>
+
+        </div>
+
       </div>
       <div className="image">
         <img src="src/assets/me.jpg" alt="A picture of me" />
