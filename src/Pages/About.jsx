31390263b27cb02e0f5a00dc2453a90ab3466182
Alex Zimmerman <ZimmermanAlex13@gmail.com>
2023-05-15 22:57:23 -0400

    incorporated a picture

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,9 +3,14 @@ import '../App.css'
 
 function About() {
   return (
-    <div className="App">
-      <div className="container">
+    <div>
+
       <h1 >About</h1>
+      <div className="image_and_text">
+      <div className='image'>
+        <img src="src/assets/grad_me.jpg" alt="A picture of me" />
+      </div>
+      <p>I'm a self-taught developer and Finance Major at the University of Massachusetts Amherst.</p>
       </div>
     </div>
   )
